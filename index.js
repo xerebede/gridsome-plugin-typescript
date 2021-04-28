949023371f6a27c41d224c3561e8c3c53c3cab07
Caleb Clark <calebjclark@gmail.com>
2021-04-28 09:31:49 -0400

    added transpileOnly + more intelligent vue.esm.js path

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+const { resolve } = require('path');
+const { existsSync } = require('fs');
+
 function TypescriptPlugin (api) {
   api.chainWebpack((config) => {
     config.module
       .rule('typescript')
-        .test(/\.tsx?$/)
-        .use()
-          .loader('ts-loader')
-          .options({ appendTsSuffixTo: [/\.vue$/] });
+      .test(/\.tsx?$/)
+      .use()
+      .loader('ts-loader')
+      .options({ appendTsSuffixTo: [/\.vue$/], transpileOnly: true });
 
     config.resolve.extensions
       .add('.ts');
 
     config.resolve.alias
-      .set('vue$', require.resolve('vue/dist/vue.esm.js'));
+      .set('vue$', vueEsmPath());
   });
+}
+
+function vueEsmPath() {
+  const vueEsmPaths = [];
+  try {
+    vueEsmPaths.push(require.resolve('vue/dist/vue.esm.js'));
+  } catch (e) {}
+  vueEsmPaths.push(resolve(process.cwd(), './node_modules/vue/dist/vue.esm.js'));
+  vueEsmPaths.push(resolve(__dirname, '../vue/dist/vue.esm.js'));
+
+  for (const vueEsmPath of vueEsmPaths) {
+    if (existsSync(vueEsmPath)) return vueEsmPath;
+  }
 }
 
 module.exports = TypescriptPlugin;
